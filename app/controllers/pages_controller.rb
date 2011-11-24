b01e80843738ac6bd673183517dcd6c087b17506
rahmani <qurandecoder@gmail.com>
2011-11-23 23:09:34 -0800

    Done with static page, alhamdulillah

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,14 @@
 class PagesController < ApplicationController
   def home
+      @title = "Home Salaam"
   end
 
   def contact
+      @title = "Contact Salaam"
   end
 
+
+  def about
+      @title = "About Salaam"
+  end
 end
